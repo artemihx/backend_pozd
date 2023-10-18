9ee9e0bac16910461aeac0c5f8de6d899b1ef844
artemihx <117065473+artemihx@users.noreply.github.com>
2023-10-18 23:14:22 +0700

    Основы PHP

--- project/public/phpBasic/task14.php
+++ project/public/phpBasic/task14.php
@@ -1,24 +1,25 @@
 <?php
 
 //Замените каждый элемент произведением всех других элементов
-// не сделал
+
 $str = '1 2 3';
 $array = explode(' ', $str);
 echo $str . '<br>';
 
-foreach ($array as $i => $n1)
-{
-    $product = 1;
-    foreach ($array as $j => $n2)
+$tempNums = [];
+    for($i = 0; $i < count($array); $i++)
     {
-        if($i === $j)
+        $product = 1;
+        for($j = 0; $j < count($array); $j++)
         {
-            continue;
+            if($i != $j)
+            {
+                $product *= $array[$j];
+            }
         }
-        $product = $product * $n2;
+        $tempNums[] = $product;
     }
-    $array[$i] = $product;
-}
+    $array = $tempNums;
 
 foreach ($array as $item) {
     echo $item .' ';
